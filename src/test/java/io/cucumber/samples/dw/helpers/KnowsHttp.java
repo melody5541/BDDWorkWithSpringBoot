b430589728ae0848e46119a614efe4e99e5adb97
melody5541 <goooooooooodboy@icloud.com>
2017-09-01 01:53:28 +0800

    remove useless code

--- src/test/java/io/cucumber/samples/dw/helpers/KnowsHttp.java
+++ src/test/java/io/cucumber/samples/dw/helpers/KnowsHttp.java
@@ -4,7 +4,6 @@ import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
-import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
@@ -18,7 +17,7 @@ public class KnowsHttp {
         RestAssured.baseURI = "http://localhost:8080";
 
         RestAssured.requestSpecification = new RequestSpecBuilder()
-                .addFilter(new RequestLoggingFilter(LogDetail.PARAMS))
+//                .addFilter(new RequestLoggingFilter(LogDetail.PATH)))
                 .addFilter(new ResponseLoggingFilter(LogDetail.BODY))
                 .build();
 
